e7da457b37097a89a4bf2f4b5c2af4e46ec30969
Chris <q.chris.d@gmail.com>
2018-04-03 22:29:36 -0400

    Updates driver to PANDAS library protocols

--- temperatureMonitor.py
+++ temperatureMonitor.py
@@ -1,8 +1,20 @@
+"""This driver utilizes the PANDAS library to achieve the necessary
+data organization. The average run time for this file to collect data:
+
+Probes		Time(s)		Date
+2		6		04-03-2018
+
+This particular driver may not be the optimal solution in terms of
+efficiency however the program is much more readable.
+"""
+
+
 # Import necessary libraries
 import csv
 import os
-import glob
 import time
+import pandas
+import re
 
 # Initializes necessary files
 os.system('modprobe w1-gpio')
@@ -12,53 +24,36 @@ os.system('modprobe w1-therm')
 now = time.strftime('%H:%M')
 today = time.strftime('%Y-%m-%d')
 base_dir = '/sys/bus/w1/devices/'
-device_config = list()
-with open('/home/pi/pi-temp-sensor/devices.csv') as f:
-	device_config = list(csv.reader(f))
-device_config = device_config[1:]
 
-# Appends a list of device files to the device_config file
-for row in device_config:
-		row.append(base_dir+row[1]+'/w1_slave')
+# Attempts to impliment pandas CSV reader
+device_config = pandas.read_csv('/home/pi/pi-temp-sensor/devices.csv')
 
-#debugging
-#print("Config File Listings:")
-#for row in device_config:
-#	print(row)
-#print
+# Appends a list of device files to the device_config file as a new column 'file'
+device_config['file'] = base_dir+device_config['Serial Number']+'/w1_slave'
 
-# Returns lines for a single file
-def read_temp_raw(file):
-	f = open(file, 'r')
-	lines = f.readlines()
-	f.close()
-	return lines
+# Collects the temperature from a single w-1 slave
+def find_temp(file):
+	temp_regex = re.compile(r'\d{5}')
+	with open(file, 'r') as f:
+		lines = f.read()
+	temp_string = temp_regex.search(lines)
+	return float(temp_string.group())/1000
 
-# Collects the temperature from single w-1 file
-def read_temps(file):
-	lines = read_temp_raw(file)
-	while lines[0].strip()[-3:] != 'YES':
-		lines = read_temp_raw(file)
-	equals_pos = lines[1].find('t=')
-	if equals_pos != -1:
-		temp_string = lines[1][equals_pos+2:]
-		temp_c = float(temp_string)/1000
-		return temp_c
-
-#debugging driver
-#for row in device_config:
-#	print(today+' '+now+' '+row[0]+': '+str(read_temps(row[2])))
+# Driver Debugging
+print('Compiled with no issue')
+for f in device_config['file']:
+	print(find_temp(f))
 
 # Write to a csv file
 header = ['Date', 'Time', 'Sensor', 'TempC']
-for file in device_config:
-	with open('/home/pi/logfiles/'+file[1]+'log.csv', 'a+') as f:
+for index, row in device_config.iterrows():
+	with open('/home/pi/pi-temp-sensor-pandas/logfiles/'+row['Serial Number']+'log.csv', 'a+') as f:
 		writer = csv.writer(f)
 		reader = csv.reader(f)
 		try:
 			next(reader)
 		except:
 			writer.writerow(header)
-		temp_row = today,now,file[0],read_temps(file[2])
+		temp_row = today,now,row['Name'],find_temp(row['file'])
 		writer.writerow(temp_row)
-#		print('written to /home/pi/'+file[1]+'log.csv')
+		print('written to /home/pi/pi-temp-sensor-pandas/logfiles'+row['Serial Number']+'log.csv')
