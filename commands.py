54124269b4ef596a7496c5664f7c7e298244ff32
qchrisd <q.chris.d@gmail.com>
2018-06-02 13:32:48 -0400

    Adds refresh commands to finished script.

--- commands.py
+++ commands.py
@@ -39,3 +39,22 @@ def triage(message):
 ''' Print command on .../printtext topic '''
 def printtext(payloadstr, topic, message):
 	print(payloadstr)
+
+''' Refreshes the status of the temperatures on topic .../thermostat/status '''
+def thermostat_refresh_status(payloadstr, topic, message):
+	# imports the status script for use
+	import status
+
+	# publishes the current status to the ballomare/thermostat/status topic
+	status.publishMQTT(status.getStatus(),"ballomare/thermostat/status")
+	print('Status published.')
+
+''' Refreshes the temperatures '''
+def thermostat_refresh_temperature(payloadstr, topic, message):
+	# imports os module and globals
+	import os
+	from globalVars import home_dir,install_dir
+
+	# runs the temp monitor script
+	os.system('python3 '+ home_dir + install_dir + 'temperatureMonitor.py')
+	print('Refreshed temperatures.')
